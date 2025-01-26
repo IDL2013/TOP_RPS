d0fc635302096622ff6bf8baa474247020a16e20
Iain Leslie <4786415+IDL2013@users.noreply.github.com>
2025-01-26 22:16:07 +0000

    Create pseudocode

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+
+// PRINT script linkage with "Hello World!"
+console.log("Hello, world!")
+
+// CREATE variable for computer choice
+// CREATE variable for human choice
+// CREATE variable for number of rounds played
+// CREATE variable for human rounds won
+
+// SELECT random number from 0-1 and multiply by 3, before rounding up
+
+// TRANSLATE number into corresponding weapon for computer (1=Rock; 2=Paper; 3=Scissors)
+
+// DISPLAY message "Choose your weapon: Rock; Paper; Scissors!"
+
+// GET user input as string
+
+// COMPARE computer and user selections: rock beats scissors; scissor beats paper; paper beats rock
+
+// PRINT statement if human wins: "Human wins! 1:0 to humans!"
+
+// PRINT statement if computer wins: "The computer wins this round! Humans are losing 0:1!"
+
+// INCREASE round number by 1
+
+// LOOP through the gams until row number = 5
+
+// IF row number = 5 and human_rounds = 5 - comp_rounds, display game conclusion statement
+
+// IF humans won the game, print "Humans win!"
+
+// ELSE if computers won the game, print "Glory to the machine!"
